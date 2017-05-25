fbc5bb816da846d9f63e76f177f95a36ae3dc395
Sean12697 <seandavidomahoney@gmail.com>
2017-05-25 22:48:50 +0100

    .

--- CalculatePi/CalculatePi.cpp
+++ CalculatePi/CalculatePi.cpp
@@ -1,11 +1,42 @@
-// CalculatePi.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
+#include <iostream>
+#include <random>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
 
+using namespace std;
+void InitRandom();
+double Random();
+
+mt19937 randGen;
 
 int main()
 {
-    return 0;
+	InitRandom();
+	long int nTrials = 0;
+	long int hitCount = 0;
+	clock_t time0 = clock();
+	while (clock() - time0 < 5 * CLOCKS_PER_SEC) {
+		double x = Random();
+		double y = Random();
+		if (x*x + y*y <= 1.0) {
+			hitCount++;
+		}
+		nTrials++;
+	}
+	std::cout << "Number of hits: " << hitCount << std::endl;
+	std::cout << "Number of cycles: " << nTrials << std::endl;
+	std::cout << "Pi?: " << (double) 4.0 * hitCount / nTrials << std::endl;
+	return 0;
 }
 
+void InitRandom()
+{
+	randGen = mt19937(16042079);
+}
+
+double Random()
+{
+	return (double)randGen() / randGen.max();
+}
